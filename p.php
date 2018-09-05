f9604a96179888bb46c0602d8da748c680b55642
vtwen <2353090160@qq.com>
2018-09-05 19:12:10 +0800

    规范化风格

---
+++ p.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wen
+ * Date: 2018-09-05
+ * Time: 18:26
+ */
+if (!function_exists('vd')){
+    function vd ($s){
+        echo "<pre>";
+        print_r($s);
+    }
+}
